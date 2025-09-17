37770bad44735721c47f6d55cc736d43f6a0d8af
ALEX <cavjob@ya.ru>
2025-09-17 06:31:42 +0300

    кастомн. тип польз.полей в lab

---
+++ public_html/local/app/UserTypes/FormatHistoryLink.php
@@ -0,0 +1,27 @@
+<?php
+namespace UserTypes;
+
+use Bitrix\Main\UserField\Types\StringFormattedType;
+use CUserTypeManager;
+
+class FormatHistoryLink extends StringFormattedType
+{
+    public const
+        USER_TYPE_ID = 'telegram_string_formatted_link',
+        RENDER_COMPONENT = 'lab:main.field.stringformatted'; // компонент который обрабатывает ссылку на телеграм
+
+    // public const RENDER_COMPONENT = 'otus:main.field.stringformatted';
+
+    public static function getDescription(): array {
+        return [
+            'DESCRIPTION' => 'Ссылка на историю обращений',
+            'BASE_TYPE' => CUserTypeManager::BASE_TYPE_STRING,
+        ];
+    }
+
+    public static function getDbColumnType(): string
+    {
+        return 'text';
+    }
+
+}
