ed0e725ae8fd9228aa2ab7b25b217f419845a1c0
ALEX <cavjob@ya.ru>
2025-09-23 18:24:59 +0300

    компонент для наполнения закладки гараж в виде таблицы компонента grid

---
+++ public_html/local/components/lab/garageauto.grid/lang/ru/class.php
@@ -0,0 +1,6 @@
+<?php
+$MESS['BOOK_GRID_BOOK_TITLE_LABEL'] = 'Марка автомобиля';
+$MESS['BOOK_GRID_BOOK_MODEL_LABEL'] = 'Модель автомобиля';
+$MESS['BOOK_GRID_BOOK_PUBLISHING_YEAR_LABEL'] = 'Год выпуска';
+$MESS['BOOK_GRID_BOOK_COLOR_LABEL'] = 'Цвет кузова';
+$MESS['BOOK_GRID_BOOK_MILEAGE_LABEL'] = 'Пробег';
